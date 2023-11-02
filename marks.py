40f024fbbb4bfbc797f2becc3a6ad98ade9c2dd2
Raza <razarais28@gmail.com>
2023-11-02 22:18:17 +0500

    Grading system

---
+++ marks.py
@@ -0,0 +1,25 @@
+# Taking input by user (marks)
+marks=int(input("Enter Your Marks:"))
+# conditions for grade::
+if 100>=marks>=88:
+    print("You Achieved A Grade!")
+elif  87>=marks>=80:
+    print("You Achieved A- Grade!")   
+elif  79>=marks>=75:
+    print("You Achieved B+ Grade!")   
+elif  74>=marks>=70:
+    print("You Achieved B Grade!")       
+elif  69>=marks>=67:
+    print("You Achieved B- Grade!")       
+elif  66>=marks>=64:
+    print("You Achieved C+ Grade!")       
+elif  63>=marks>=60:
+    print("You Achieved C Grade!")       
+elif  59>=marks>=57:
+    print("You Achieved C- Grade!")       
+elif  56>=marks>=54:
+    print("You Achieved D+ Grade!")       
+elif  53>=marks>=50:
+    print("You Achieved D Grade!")     
+else:
+    print("pass,You got no Grade")    
