f619f14c382d8fe7e4cdf39ee6a2c32d656f70ad
Raza <razarais28@gmail.com>
2023-11-03 19:46:13 +0500

    Average calculation

---
+++ average.py
@@ -0,0 +1,12 @@
+# Taking input:
+mark1=int(input("Enter value of first integer:"))
+mark2=int(input("Enter value of second integer:"))
+mark3=int(input("Enter value of third integer:"))
+mark4=int(input("Enter value of fourth integer:"))
+mark5=int(input("Enter value of fifth integer:"))
+# Finding Average:
+result=(mark1+mark2+mark3+mark4+mark5)/5
+# printing result:
+print(f"The average of 5 integer values are:{result}")
+
+
