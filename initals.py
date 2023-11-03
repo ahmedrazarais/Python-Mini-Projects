10f7fb5674610b3be223e9bbd6c53aa79bb3492c
Raza <razarais28@gmail.com>
2023-11-03 19:47:11 +0500

    Initials of name

---
+++ initals.py
@@ -0,0 +1,12 @@
+# Taking input of name:
+first=(input("Enter Your First Name:")) 
+last=(input("Enter Your Last Name:"))
+# For all capital letter:
+capsf=first. upper()
+capsl=last.upper()
+# finding initials:
+initial1=capsf[0]
+initial2=capsl[0]
+# printing result:
+print(f"Initials of your name are: {initial1}.{initial2}.")
+
