766aa0bd424deb5bb0426da71202cbd368bdbf54
Raza <razarais28@gmail.com>
2023-11-19 02:03:57 +0500

    factors of number

---
+++ factors.py
@@ -0,0 +1,10 @@
+# FINDING FACTORS OF NUMBER:
+# TAKING INPUT BY USER:
+num=int(input("Enter The Integer:"))
+# APLYING LOOP FOR CHECKING FACTORS OF NUMBER:
+for i in range(1,num+1):
+        if num%i==0:
+            # pRITING RESULT:
+                print(f" Factors of {num} are : {i}")                
+        
+
