843096a36f2321bb315267700266010fe14dfe74
Raza <razarais28@gmail.com>
2023-11-19 03:15:15 +0500

    sum

---
+++ sumloop.py
@@ -0,0 +1,17 @@
+# Task 1: Write a program that calculates the sum of squares of numbers from 1 to 10 using a for loop.
+# Hint: Use the range() function to generate a sequence of numbers from 1 to 10. Use a for loop to iterate through the sequence and calculate the square of each number. Add the squares to a variable to calculate the sum.
+# APPLYING LOOP:
+total=0
+for i in range(1,11):
+    square=i*i
+    total+=square
+# printing result    
+print(f"The sum of squares from 1 to 10 is {total}")    
+
+       
+
+
+
+
+
+
