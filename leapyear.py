a3bc241c309a984a1b9466cee3ca230c699f302e
Raza <razarais28@gmail.com>
2023-11-19 01:28:43 +0500

    Leap Year Checker

---
+++ leapyear.py
@@ -0,0 +1,8 @@
+# CHECKING LEAP YEAR:
+# TAKING INPUT BY USER OF YEAR:
+year=int(input("Enter The Year:"))
+# CHECKING THE LEAP YEAR:
+if year%4==0:
+    print(f"{year} Is The Leap Year")
+else:
+    print(f"{year} Is Not Leap Year")    
